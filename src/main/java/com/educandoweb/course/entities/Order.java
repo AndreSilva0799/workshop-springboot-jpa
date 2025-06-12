3922702e08573531289559e7817d3e85a03eb060
Andre Victor <andrevictor2102@hotmail.com>
2025-06-11 23:45:31 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -88,6 +88,13 @@ public class Order implements Serializable {
     }
 
 
+    public Double getTotal(){
+        double sum = 0.0;
+                for (OrderItem item : items) {
+                    sum += item.getSubTotal();
+                }
+                return sum;
+    }
 
 
     @Override
